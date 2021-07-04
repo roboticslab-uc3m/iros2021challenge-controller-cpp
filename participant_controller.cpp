23f8d3204d1132cee79b86094199ead64d5cd311
jgvictores <jcgvicto@gmail.com>
2021-07-04 20:39:51 +0200

    add velocities

--- participant_controller.cpp
+++ participant_controller.cpp
@@ -14,11 +14,19 @@ int main(int argc, char **argv)
     webots::Motor* motor_left = robot->getMotor("wheel_left_joint");
     webots::Motor* motor_right = robot->getMotor("wheel_right_joint");
 
+    motor_left->setVelocity(0.0);
+    motor_right->setVelocity(0.0);
+
+    motor_left->setPosition(std::numeric_limits<double>::infinity());
+    motor_right->setPosition(std::numeric_limits<double>::infinity());
+
     // Main loop:
     // - perform simulation steps until Webots is stopping the controller
     while (robot->step(timestep) != -1)
     {
         std::cout << "Hello World from c++!" << std::endl;
+        motor_left->setVelocity(5.0);
+        motor_right->setVelocity(5.0);
     }
 
     delete robot;
