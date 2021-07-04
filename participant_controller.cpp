83ae03e0ec036f2e947fc6adbf8d51131f2f6f0a
jgvictores <jcgvicto@gmail.com>
2021-07-04 20:09:29 +0200

    add robot step loop

--- participant_controller.cpp
+++ participant_controller.cpp
@@ -4,9 +4,16 @@
 
 int main(int argc, char **argv)
 {
+    // Initialize the robot
     webots::Robot *robot = new webots::Robot();
+    int timestep = int(robot->getBasicTimeStep());
 
-    std::cout << "Hello World from c++!" << std::endl;
+    // Main loop:
+    // - perform simulation steps until Webots is stopping the controller
+    while (robot->step(timestep) != -1)
+    {
+        std::cout << "Hello World from c++!" << std::endl;
+    }
 
     delete robot;
     return 0;
