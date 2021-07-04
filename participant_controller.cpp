60c630cd58530c4438c0491300d0a796f67a9646
jgvictores <jcgvicto@gmail.com>
2021-07-04 21:30:32 +0200

    add gps

--- participant_controller.cpp
+++ participant_controller.cpp
@@ -1,5 +1,6 @@
 #include <webots/Robot.hpp>
 #include <webots/Motor.hpp>
+#include <webots/GPS.hpp>
 
 #include <iostream>
 #include <limits>
@@ -13,6 +14,7 @@ int main(int argc, char **argv)
     // Initialize devices
     webots::Motor* motor_left = robot->getMotor("wheel_left_joint");
     webots::Motor* motor_right = robot->getMotor("wheel_right_joint");
+    webots::GPS* gps = robot->getGPS("gps");
 
     motor_left->setVelocity(0.0);
     motor_right->setVelocity(0.0);
@@ -20,11 +22,16 @@ int main(int argc, char **argv)
     motor_left->setPosition(std::numeric_limits<double>::infinity());
     motor_right->setPosition(std::numeric_limits<double>::infinity());
 
+    gps->enable(timestep);
+
     // Main loop:
     // - perform simulation steps until Webots is stopping the controller
     while (robot->step(timestep) != -1)
     {
-        std::cout << "Hello World from c++!" << std::endl;
+        const double * pos = gps->getValues();
+        std::cout << "Hello World from c++! ["
+                  << pos[0] << " " << pos[1] << " " << pos[2]
+                  << "]" << std::endl;
         motor_left->setVelocity(5.0);
         motor_right->setVelocity(5.0);
     }
