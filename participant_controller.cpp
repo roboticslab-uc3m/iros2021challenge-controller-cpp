3b4d5d2ca2bf63674da6a61120b8022e65597a81
jgvictores <jcgvicto@gmail.com>
2021-07-04 20:28:04 +0200

    add view motors

--- participant_controller.cpp
+++ participant_controller.cpp
@@ -1,12 +1,18 @@
 #include <webots/Robot.hpp>
+#include <webots/Motor.hpp>
 
 #include <iostream>
+#include <limits>
 
 int main(int argc, char **argv)
 {
     // Initialize the robot
     webots::Robot *robot = new webots::Robot();
     int timestep = int(robot->getBasicTimeStep());
+
+    // Initialize devices
+    webots::Motor* motor_left = robot->getMotor("wheel_left_joint");
+    webots::Motor* motor_right = robot->getMotor("wheel_right_joint");
 
     // Main loop:
     // - perform simulation steps until Webots is stopping the controller
